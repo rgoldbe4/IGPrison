69bba46cb682b4e2be172237cd6b363d74e8047c
BuffsOverNexus <buffsovernexus@gmail.com>
2018-06-18 20:50:42 -0400

    Added IGPrison

---
+++ IGPrison/src/us/ignitiongaming/IGPrison.java
@@ -0,0 +1,16 @@
+package us.ignitiongaming;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class IGPrison extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
