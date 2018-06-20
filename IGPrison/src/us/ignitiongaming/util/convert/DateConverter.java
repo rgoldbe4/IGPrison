ea5077ecd34b0f81e0a65f54e0cf4bb8ccf7f94c
BuildTools <unconfigured@null.spigotmc.org>
2018-06-20 12:38:27 -0400

    Fixed Date Converter Issue #3

--- IGPrison/src/us/ignitiongaming/util/convert/DateConverter.java
+++ IGPrison/src/us/ignitiongaming/util/convert/DateConverter.java
@@ -145,7 +145,7 @@ public class DateConverter {
 		date = date.split("-")[1] + "/" + date.split("-")[2] + "/" + date.split("-")[0];
 		int hour = Integer.parseInt(time.split(":")[0]);
 		boolean isAfternoon = false;
-		
+		if (hour == 0) hour = 12;
 		if (hour > 12) {
 			hour = hour - 12;
 			isAfternoon = true;
