5be7d982119c6e011c930e4c81c3cd93d49cc0f5
BuildTools <unconfigured@null.spigotmc.org>
2018-06-21 11:31:50 -0400

    clears specific named entities on player death

--- IGPrison/src/us/ignitiongaming/command/GuardBatonCommand.java
+++ IGPrison/src/us/ignitiongaming/command/GuardBatonCommand.java
@@ -1,5 +1,7 @@
 package us.ignitiongaming.command;
 
 public class GuardBatonCommand {
-
+	public static final String BATON_NAME = "Guard Baton";
+	public static final String ARMOR_NAME = "Guard Armor";
+	public static final String STICK_NAME = "Solitary Stick";
 }
