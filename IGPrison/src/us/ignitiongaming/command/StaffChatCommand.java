3b5228c66f0d46bcd420379e08c0d82a2741819e
BuffsOverNexus <buffsovernexus@gmail.com>
2018-06-20 21:09:34 -0400

    Small logic changes to staff chat.

--- IGPrison/src/us/ignitiongaming/command/StaffChatCommand.java
+++ IGPrison/src/us/ignitiongaming/command/StaffChatCommand.java
@@ -8,10 +8,9 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import us.ignitiongaming.entity.player.IGPlayer;
+import us.ignitiongaming.database.ConvertUtils;
 import us.ignitiongaming.entity.rank.IGRank;
 import us.ignitiongaming.enums.IGRanks;
-import us.ignitiongaming.factory.player.IGPlayerFactory;
 import us.ignitiongaming.factory.rank.IGRankFactory;
 import us.ignitiongaming.singleton.IGSingleton;
 
@@ -23,19 +22,19 @@ public class StaffChatCommand implements CommandExecutor{
 			if (sender instanceof Player) {
 				IGSingleton igs = IGSingleton.getInstance();
 				Player player = (Player) sender;
-				IGPlayer igPlayer = IGPlayerFactory.getIGPlayerByPlayer(player);
-				UUID playerID = igPlayer.getUUID();
-				if(args.length == 1){
+				UUID playerID = player.getUniqueId();
+				
+				if (lbl.equalsIgnoreCase(""))
+				if (args.length != 0){
 					IGRank staff = IGRankFactory.getIGRankByRank(IGRanks.STAFF);
 					IGRank guard = IGRankFactory.getIGRankByRank(IGRanks.STAFF);
 					IGRank warden = IGRankFactory.getIGRankByRank(IGRanks.STAFF);
-					for (Player player2 : Bukkit.getOnlinePlayers()){
-						if(player2.hasPermission(staff.getNode()))player2.sendMessage("§a[StaffChat] §r" + igPlayer.getName() + ": " + args[0]);
-						if(player2.hasPermission(guard.getNode()))player2.sendMessage("§a[StaffChat] §r" + igPlayer.getName() + ": " + args[0]);
-						if(player2.hasPermission(warden.getNode()))player2.sendMessage("§a[StaffChat] §r" + igPlayer.getName() + ": " + args[0]);	
+					for ( Player online : Bukkit.getOnlinePlayers() ){
+						if(online.hasPermission(staff.getNode()) || online.hasPermission(guard.getNode()) || online.hasPermission(warden.getNode())) 
+							online.sendMessage("§b" + player.getName() + ": " + ConvertUtils.getStringFromCommand(1, args));
 					}
 				}
-				else{
+				else {
 					igs.toggleStaffChatter(playerID);
 				}
 			}
