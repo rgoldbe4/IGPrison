1e65843fe00e20d2e222305d4c4bb20ebb474ed6
BuffsOverNexus <buffsovernexus@gmail.com>
2018-06-22 00:52:59 -0400

    [#15] Yolo

--- IGPrison/src/us/ignitiongaming/command/IGSKickBanCommand.java
+++ IGPrison/src/us/ignitiongaming/command/IGSKickBanCommand.java
@@ -8,12 +8,14 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import us.ignitiongaming.database.ConvertUtils;
 import us.ignitiongaming.entity.rank.IGRank;
 import us.ignitiongaming.enums.IGRanks;
 import us.ignitiongaming.factory.player.IGPlayerBannedFactory;
 import us.ignitiongaming.factory.player.IGPlayerFactory;
 import us.ignitiongaming.factory.player.IGPlayerKickedFactory;
 import us.ignitiongaming.factory.rank.IGRankFactory;
+import us.ignitiongaming.util.convert.DateConverter;
 
 public class IGSKickBanCommand implements CommandExecutor {
 
@@ -25,34 +27,30 @@ public class IGSKickBanCommand implements CommandExecutor {
 			Player kicker = (Player) sender;
 			if(lbl.contains("ban") && args.length == 3){
 				Player kicked = Bukkit.getPlayer(args[0]);
-				int days = 0;
-				int hours = 0;
-				int minutes = 0;
-				int seconds = 0;
-				long start  = System.currentTimeMillis();
-				for(String s : args[1].split("|")){
-					if(s.endsWith("d"))days =  Integer.parseInt(s.substring(0, s.length()-2));
-					if(s.endsWith("h"))hours = Integer.parseInt(s.substring(0, s.length()-2));
-					if(s.endsWith("m"))minutes = Integer.parseInt(s.substring(0, s.length()-2));
-					if(s.endsWith("s"))seconds = Integer.parseInt(s.substring(0, s.length()-2));
-				}
-				long end = start + ((((days*24)+hours)*60+minutes)*60+seconds)*1000;
-				Date startDate = new Date(start);
-				Date endDate = new Date(end);
-				IGPlayerBannedFactory.add(IGPlayerFactory.getIGPlayerByPlayer(kicked), startDate, endDate, args[2], IGPlayerFactory.getIGPlayerByPlayer(kicker));
-				String message = kicker.getName() + " banned " + args[0] + " for " + days + " days, " +  hours + " hours, " + minutes + " minutes, and" + seconds + " seconds for " + args[2];
+				String reason = ConvertUtils.getStringFromCommand(2, args);
+				Date startDate = DateConverter.getCurrentTime();
+				Date endDate = DateConverter.getCurrentTime();
+				
+				//Convert
+				endDate = DateConverter.convertSingleArgumentContextToDate(args[1]);
+				
+				Bukkit.broadcastMessage("Start: " + DateConverter.toFriendlyDate(startDate));
+				Bukkit.broadcastMessage("End: " + DateConverter.toFriendlyDate(endDate));
+				Bukkit.broadcastMessage("Differs By: " + DateConverter.compareDatesFriendly(startDate, endDate));
+				
+				IGPlayerBannedFactory.add(IGPlayerFactory.getIGPlayerByPlayer(kicked), startDate, endDate, reason, IGPlayerFactory.getIGPlayerByPlayer(kicker));
 				if (lbl.contains("s")){
 					IGRank staff = IGRankFactory.getIGRankByRank(IGRanks.STAFF);
 					IGRank guard = IGRankFactory.getIGRankByRank(IGRanks.STAFF);
 					IGRank warden = IGRankFactory.getIGRankByRank(IGRanks.STAFF);
 					for ( Player online : Bukkit.getOnlinePlayers() ){
 						if(online.hasPermission(staff.getNode()) || online.hasPermission(guard.getNode()) || online.hasPermission(warden.getNode())) 
-							online.sendMessage("§b§l" +message);
+							online.sendMessage("§b§l" + DateConverter.compareDatesFriendly(startDate, endDate));
 					}
 				}
 				else{
 					for ( Player online : Bukkit.getOnlinePlayers() ){
-							online.sendMessage(message);
+							online.sendMessage(DateConverter.compareDatesFriendly(startDate, endDate));
 					}
 				}
 			}
