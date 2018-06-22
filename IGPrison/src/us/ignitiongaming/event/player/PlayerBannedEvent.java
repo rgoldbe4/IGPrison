a635725f1d91b9956e020e00459c85399a628fdc
BuildTools <unconfigured@null.spigotmc.org>
2018-06-22 00:30:58 -0400

    Issue #12 complete

--- IGPrison/src/us/ignitiongaming/event/player/PlayerBannedEvent.java
+++ IGPrison/src/us/ignitiongaming/event/player/PlayerBannedEvent.java
@@ -7,6 +7,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import us.ignitiongaming.entity.player.IGPlayer;
 import us.ignitiongaming.factory.player.IGPlayerBannedFactory;
 import us.ignitiongaming.factory.player.IGPlayerFactory;
 
@@ -14,9 +15,12 @@ public class PlayerBannedEvent implements Listener {
 	@EventHandler
 	public static void onPlayerJoin(PlayerJoinEvent event){
 		Player player = event.getPlayer();
-		Date banDate = IGPlayerBannedFactory.isBanned(IGPlayerFactory.getIGPlayerByPlayer(player));
-		if(banDate != null){
-			player.kickPlayer("You have been banned until " + banDate.toString());
+		IGPlayer igPlayer = IGPlayerFactory.getIGPlayerByPlayer(player);
+		if(igPlayer != null){
+			Date banDate = IGPlayerBannedFactory.isBanned(igPlayer);
+			if(banDate != null){
+				player.kickPlayer("You have been banned until " + banDate.toString());
+			}
 		}
 	}
 }
