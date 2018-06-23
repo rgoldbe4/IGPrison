a16d98b21299ebde699624c26dd4ecba74fe2f0f
BuffsOverNexus <buffsovernexus@gmail.com>
2018-06-23 19:04:46 -0400

    [#15] REEEEE

--- IGPrison/src/us/ignitiongaming/event/player/PickaxeDamageEvent.java
+++ IGPrison/src/us/ignitiongaming/event/player/PickaxeDamageEvent.java
@@ -1,5 +1,6 @@
 package us.ignitiongaming.event.player;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -10,19 +11,20 @@ import org.bukkit.inventory.ItemStack;
 
 public class PickaxeDamageEvent implements Listener {
 	
-	@SuppressWarnings("deprecation")
 	@EventHandler
 	public void onPlayerDamagePlayer(EntityDamageByEntityEvent event) {
 		try {
 		Entity damager = event.getDamager();
 		Entity damaged = event.getEntity();
-		event.getCause();
-		if(damager instanceof Player && damaged instanceof Player){
+		if (damager instanceof Player && damaged instanceof Player) {
 			Player damagingPlayer = (Player) damager;
-			if(event.getCause().equals(EntityDamageEvent.DamageCause.ENTITY_SWEEP_ATTACK)|| event.getCause().equals(EntityDamageEvent.DamageCause.ENTITY_ATTACK)) {
-				ItemStack held = damagingPlayer.getItemInHand();
-				if(held != null)
-					if(held.getTypeId() == 270 || held.getTypeId() == 274 || held.getTypeId() == 257 || held.getTypeId() == 285 || held.getTypeId() == 278)
+			if (event.getCause().equals(EntityDamageEvent.DamageCause.ENTITY_SWEEP_ATTACK)|| event.getCause().equals(EntityDamageEvent.DamageCause.ENTITY_ATTACK)) {
+				ItemStack held = damagingPlayer.getInventory().getItemInMainHand();
+				if (held != null)
+					if (   held.getType() == Material.DIAMOND_PICKAXE
+					    || held.getType() == Material.IRON_PICKAXE 
+					    || held.getType() == Material.WOOD_PICKAXE
+					    || held.getType() == Material.GOLD_PICKAXE )
 						event.setCancelled(true);
 			}
 			
