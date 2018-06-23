596b7ead1c6b0fd571e4d4896f2ff3b8136366e1
BuffsOverNexus <buffsovernexus@gmail.com>
2018-06-23 19:07:31 -0400

    [#15] Spacing is important.

--- IGPrison/src/us/ignitiongaming/event/player/PickaxeDamageEvent.java
+++ IGPrison/src/us/ignitiongaming/event/player/PickaxeDamageEvent.java
@@ -14,21 +14,21 @@ public class PickaxeDamageEvent implements Listener {
 	@EventHandler
 	public void onPlayerDamagePlayer(EntityDamageByEntityEvent event) {
 		try {
-		Entity damager = event.getDamager();
-		Entity damaged = event.getEntity();
-		if (damager instanceof Player && damaged instanceof Player) {
-			Player damagingPlayer = (Player) damager;
-			if (event.getCause().equals(EntityDamageEvent.DamageCause.ENTITY_SWEEP_ATTACK)|| event.getCause().equals(EntityDamageEvent.DamageCause.ENTITY_ATTACK)) {
-				ItemStack held = damagingPlayer.getInventory().getItemInMainHand();
-				if (held != null)
-					if (   held.getType() == Material.DIAMOND_PICKAXE
-					    || held.getType() == Material.IRON_PICKAXE 
-					    || held.getType() == Material.WOOD_PICKAXE
-					    || held.getType() == Material.GOLD_PICKAXE )
-						event.setCancelled(true);
+			Entity damager = event.getDamager();
+			Entity damaged = event.getEntity();
+			if (damager instanceof Player && damaged instanceof Player) {
+				Player damagingPlayer = (Player) damager;
+				if (event.getCause().equals(EntityDamageEvent.DamageCause.ENTITY_SWEEP_ATTACK)|| event.getCause().equals(EntityDamageEvent.DamageCause.ENTITY_ATTACK)) {
+					ItemStack held = damagingPlayer.getInventory().getItemInMainHand();
+					if (held != null)
+						if (   held.getType() == Material.DIAMOND_PICKAXE
+						    || held.getType() == Material.IRON_PICKAXE 
+						    || held.getType() == Material.WOOD_PICKAXE
+						    || held.getType() == Material.GOLD_PICKAXE )
+							event.setCancelled(true);
+				}
+				
 			}
-			
-		}
 		}
 		catch (Exception e) {
 			e.printStackTrace();
