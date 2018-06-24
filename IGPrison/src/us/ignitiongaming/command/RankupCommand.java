97ec3ea9c2ff2ad2b323782215792cb400cbb368
BuffsOverNexus <buffsovernexus@gmail.com>
2018-06-23 22:36:17 -0400

    [#32] Added /rankup

--- IGPrison/src/us/ignitiongaming/command/RankupCommand.java
+++ IGPrison/src/us/ignitiongaming/command/RankupCommand.java
@@ -7,8 +7,12 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import us.ignitiongaming.config.GlobalMessages;
+import us.ignitiongaming.config.GlobalTags;
+import us.ignitiongaming.config.ServerDefaults;
+import us.ignitiongaming.enums.IGRankNodes;
+import us.ignitiongaming.enums.IGSettings;
 
-public class RankupCommand implements CommandExecutor{
+public class RankupCommand implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String lbl, String[] args) {
@@ -17,15 +21,66 @@ public class RankupCommand implements CommandExecutor{
 				Player player = (Player) sender;
 				// [/rankup]
 				if (lbl.equalsIgnoreCase("rankup")) {
-					player.sendMessage(GlobalMessages.UNDER_CONSTRUCTION);
+					double playerBalance = ServerDefaults.econ.getBalance(player);
+					IGRankNodes playerRank = IGRankNodes.getPlayerRank(player);
+					
+					switch (playerRank) {
+					case D:
+						double rankupCostD = Double.parseDouble(IGSettings.RANKUP_D.toSetting().getValue().toString());
+						if ( playerBalance >= rankupCostD) {
+							Bukkit.broadcastMessage(GlobalTags.RANKUP + player.getName() + " has ranked up to " + IGRankNodes.C.getTag());
+							player.sendMessage("§8Your balance is now: §a$" + (playerBalance - rankupCostD));
+							Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "pex user " + player.getName() + " group set c");
+						} else {
+							player.sendMessage(GlobalTags.RANKUP + "§cYou need §a$" + (rankupCostD - playerBalance) + "§c to rankup.");
+						}
+						break;
+					case C:
+						double rankupCostC = Double.parseDouble(IGSettings.RANKUP_C.toSetting().getValue().toString());
+						if ( playerBalance >= rankupCostC) {
+							Bukkit.broadcastMessage(GlobalTags.RANKUP + player.getName() + " has ranked up to " + IGRankNodes.B.getTag());
+							player.sendMessage("§8Your balance is now: §a$" + (playerBalance - rankupCostC));
+							Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "pex user " + player.getName() + " group set b");
+						} else {
+							player.sendMessage(GlobalTags.RANKUP + "§cYou need §a$" + (rankupCostC - playerBalance) + "§c to rankup.");
+						}
+						break;
+					case B:
+						double rankupCostB = Double.parseDouble(IGSettings.RANKUP_B.toSetting().getValue().toString());
+						if ( playerBalance >= rankupCostB) {
+							Bukkit.broadcastMessage(GlobalTags.RANKUP + player.getName() + " has ranked up to " + IGRankNodes.A.getTag());
+							player.sendMessage("§8Your balance is now: §a$" + (playerBalance - rankupCostB));
+							Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "pex user " + player.getName() + " group set a");
+						} else {
+							player.sendMessage(GlobalTags.RANKUP + "§cYou need §a$" + (rankupCostB - playerBalance) + "§c to rankup.");
+						}
+						break;
+					case A:
+						double rankupCostA = Double.parseDouble(IGSettings.RANKUP_A.toSetting().getValue().toString());
+						if ( playerBalance >= rankupCostA) {
+							Bukkit.broadcastMessage(GlobalTags.RANKUP + player.getName() + " has ranked up to " + IGRankNodes.FREE.getTag());
+							player.sendMessage("§8Your balance is now: §a$" + (playerBalance - rankupCostA));
+							Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "pex user " + player.getName() + " group set free");
+						} else {
+							player.sendMessage(GlobalTags.RANKUP + "§cYou need §a$" + (rankupCostA - playerBalance) + "§c to rankup.");
+						}
+						break;
+					case FREE:
+					case GUARD:
+					case WARDEN:
+					case STAFF:
+					case SOLITARY:
+					default:
+						player.sendMessage("You cannot rankup any higher.");
+						break;
+					}
 				}
 				
 				// [/setrank]
 				if (lbl.equalsIgnoreCase("setrank")) {
-					player.sendMessage(GlobalMessages.UNDER_CONSTRUCTION);
 					if (args.length == 2) {
 						if (player.hasPermission("igprison.staff")) {
-							Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "pex user " + args[0] + " group add " + args[1]);
+							Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "pex user " + args[0] + " group set " + args[1]);
 							player.sendMessage(args[0] + " has been added to " + args[1]);
 							
 						} else {
--- IGPrison/src/us/ignitiongaming/config/GlobalTags.java
+++ IGPrison/src/us/ignitiongaming/config/GlobalTags.java
@@ -6,4 +6,6 @@ public class GlobalTags {
 	public static final String SOLITARY = "§8[§7Solitary§8] §r";
 	public static final String LOCKDOWN = "§8§l[§4LOCKDOWN§8§l] §r";
 	
+	public static final String RANKUP = "§1[§fRankup§1] §r";
+	
 }
