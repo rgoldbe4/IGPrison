48d8f59c3ee6f6654fd91f8bbd0dd8d27a1a7247
BuildTools <unconfigured@null.spigotmc.org>
2018-06-24 14:19:25 -0400

    [#30] fully implemented clockin/out

--- IGPrison/src/us/ignitiongaming/singleton/IGSingleton.java
+++ IGPrison/src/us/ignitiongaming/singleton/IGSingleton.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 public class IGSingleton {
 	private ArrayList<UUID> staffchatters;
+	private ArrayList<UUID> clockedin;
 	private static IGSingleton uniqueInstance = new IGSingleton();
 	private IGSingleton(){
 		staffchatters = new ArrayList<UUID>();
@@ -16,7 +17,16 @@ public class IGSingleton {
 		if(staffchatters.contains(playerID))staffchatters.remove(playerID);
 		else staffchatters.add(playerID);
 	}
+	public void clockIn(UUID playerID){
+		if(!clockedin.contains(playerID))clockedin.add(playerID);
+	}
+	public void clockOut(UUID playerID){
+		if(clockedin.contains(playerID))clockedin.remove(playerID);
+	}
 	public ArrayList<UUID> getStaffChatters() {
 		return staffchatters;
+	}
+	public ArrayList<UUID> getClockedIn() {
+		return clockedin;
 	}
 }
