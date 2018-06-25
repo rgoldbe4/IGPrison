0df5a97f7f033189112c6d2d04ebfd31bd545fad
BuffsOverNexus <buffsovernexus@gmail.com>
2018-06-24 21:24:40 -0400

    [#15] Bad import.

--- IGPrison/src/us/ignitiongaming/util/items/GuardSolitaryBaton.java
+++ IGPrison/src/us/ignitiongaming/util/items/GuardSolitaryBaton.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Material;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
