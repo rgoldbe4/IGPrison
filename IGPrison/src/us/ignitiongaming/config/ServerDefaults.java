3879d8262733f9ebda0d8c1e2b8857c453f24a8b
BuffsOverNexus <buffsovernexus@gmail.com>
2018-07-22 18:31:33 -0400

    [#15] Added environment database.

--- IGPrison/src/us/ignitiongaming/config/ServerDefaults.java
+++ IGPrison/src/us/ignitiongaming/config/ServerDefaults.java
@@ -5,6 +5,7 @@ import java.util.List;
 import net.milkbowl.vault.economy.Economy;
 import us.ignitiongaming.entity.other.IGSetting;
 import us.ignitiongaming.enums.IGCells;
+import us.ignitiongaming.enums.IGEnvironments;
 import us.ignitiongaming.enums.IGLocations;
 import us.ignitiongaming.enums.IGRankNodes;
 import us.ignitiongaming.enums.IGSettings;
@@ -40,5 +41,6 @@ public class ServerDefaults {
 	public static IGCells DEFAULT_CELL = IGCells.D;
 	
 	public static Economy econ;
+	public static IGEnvironments ENVIRONMENT;
 	
 }
