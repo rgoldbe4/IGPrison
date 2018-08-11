b6028ac084e3b3c16ed146dbce89ae51d20de940
BuffsOverNexus <buffsovernexus@gmail.com>
2018-08-11 00:09:08 -0400

    Made the mistake of not switching back to development.

--- IGPrison/src/us/ignitiongaming/util/items/Drugs.java
+++ IGPrison/src/us/ignitiongaming/util/items/Drugs.java
@@ -12,20 +12,36 @@ public class Drugs {
 
 	
 	private static ItemStack cactus = new ItemStack(Material.CACTUS);
+	private static ItemStack sugarCane = new ItemStack(Material.SUGAR_CANE);
 	
 	
 	public static ItemStack getAutoDrop() {
-		/* Item Meta */
+		int drugAmount = Integer.parseInt(ServerDefaults.getSetting(IGSettings.DEFAULT_DRUG_AMOUNT).getValue().toString());
+		return getAutoDrop(drugAmount);
+	}
+	
+	public static ItemStack getAutoDrop(int amount) {
 		ItemMeta cactusMeta = cactus.getItemMeta();
 		cactusMeta.setLore(DrugLore.getAutoDropLore());
 		cactusMeta.setDisplayName(IGDrugType.AUTO_DROP.getTitle());
 		cactus.setItemMeta(cactusMeta);
-		
-		//Default drug amount
-		int drugAmount = Integer.parseInt(ServerDefaults.getSetting(IGSettings.DEFAULT_DRUG_AMOUNT).getValue().toString());
-		cactus.setAmount(drugAmount);
+		cactus.setAmount(amount);
 		
 		return cactus;
+	}
+	
+	public static ItemStack getWarrior() {
+		int drugAmount = Integer.parseInt(ServerDefaults.getSetting(IGSettings.DEFAULT_DRUG_AMOUNT).getValue().toString());
+		return getWarrior(drugAmount);
+	}
+	
+	public static ItemStack getWarrior(int amount) {
+		ItemMeta itemMeta = sugarCane.getItemMeta();
+		itemMeta.setLore(DrugLore.getWarriorLore());
+		itemMeta.setDisplayName(IGDrugType.WARRIOR.getTitle());
+		sugarCane.setItemMeta(itemMeta);
+		sugarCane.setAmount(amount);
 		
+		return sugarCane;
 	}
 }
