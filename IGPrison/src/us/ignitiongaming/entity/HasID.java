1562feca6d736cfa77023663c3b13e62db8b865f
BuffsOverNexus <buffsovernexus@gmail.com>
2018-08-21 15:45:49 -0400

    [#133] Added visibility functionality to HasID

--- IGPrison/src/us/ignitiongaming/entity/HasID.java
+++ IGPrison/src/us/ignitiongaming/entity/HasID.java
@@ -2,10 +2,14 @@ package us.ignitiongaming.entity;
 
 import java.sql.ResultSet;
 
+import us.ignitiongaming.util.convert.BooleanConverter;
+
 public class HasID {
 
 	//ID of all entities are auto-increment, which means no ID can ever be 0. Therefore, make it a checkpoint in code.
 	private int id = 0;
+	//Default the visibility to false.
+	private boolean visible = false;
 	/**
 	 * Set the ID of the entity.
 	 * @param id
@@ -20,7 +24,10 @@ public class HasID {
 	public int getId() { 
 		return id;
 	}
-	
+	/**
+	 * Does the ID exist for the entity.
+	 * @return
+	 */
 	public boolean hasId() {
 		return !(id == 0);
 	}
@@ -36,4 +43,31 @@ public class HasID {
 			id = 0;
 		}
 	}
+	
+	public void setVisible(int visible) { 
+		setVisible(BooleanConverter.getBooleanFromInteger(visible));
+	}
+	
+	public void setVisible(boolean visible) {
+		this.visible = visible;
+	}
+	
+	public void setVisible(ResultSet result) {
+		try {
+			visible = BooleanConverter.getBooleanFromInteger(result.getInt("visible"));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public int getVisible() { 
+		return BooleanConverter.getIntegerFromBoolean(visible);
+	}
+	
+	public boolean isVisible() {
+		return visible == true;
+	}
+	
+	public boolean isShowing() { return isVisible(); }
+	public boolean isHiding() { return !isShowing(); }
 }
---
+++ IGPrison/src/us/ignitiongaming/entity/player/IGPlayerHome.java
@@ -0,0 +1,48 @@
+package us.ignitiongaming.entity.player;
+
+import java.sql.ResultSet;
+
+import us.ignitiongaming.database.SQLQuery;
+import us.ignitiongaming.database.SQLQuery.QueryType;
+import us.ignitiongaming.entity.HasID;
+
+public class IGPlayerHome extends HasID {
+
+	public static final String TABLE_NAME = "player_home";
+	
+	private int playerId, homeId;
+	
+	public void setPlayerId(int playerId) { this.playerId = playerId; }
+	public int getPlayerId() { return playerId; }
+	
+	public void setHomeId(int homeId) { this.homeId = homeId; }
+	public int getHomeId() { return homeId; }
+	
+	public void assign(ResultSet results) {
+		try {
+			setId(results);
+			setPlayerId(results.getInt("playerID"));
+			setHomeId(results.getInt("homeID"));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public boolean isValid() {
+		return !(hasId() || playerId <= 0 || homeId <= 0);
+	}
+	
+	public void save() {
+		SQLQuery query = new SQLQuery(QueryType.UPDATE, TABLE_NAME);
+		query.addSet("playerID", getPlayerId());
+		query.addSet("homeID", getHomeId());
+		query.addId(getId());
+		query.execute();
+	}
+	
+	public void delete() {
+		SQLQuery query = new SQLQuery(QueryType.UPDATE, TABLE_NAME);
+		query.addSet("visible", getVisible());
+		query.addId(getId());
+	}
+}
